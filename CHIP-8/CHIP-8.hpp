c9efb5241557be787b7a7dd38e9e04dd0295f803
Abhishek Kumar <abhi.kr.2100@gmail.com>
2023-12-10 05:29:50 +0530

    Add CHIP-8 specific descriptive types

--- CHIP-8/CHIP-8.hpp
+++ CHIP-8/CHIP-8.hpp
@@ -5,6 +5,10 @@
 
 #include "font-data.hpp"
 
+using byte = std::uint8_t;
+using double_byte = std::uint16_t;
+using instruction_t = double_byte;
+
 constexpr auto MEMORY_SIZE = 4096 /* bytes */;
 constexpr auto STACK_SIZE = 32 /* bytes */;
 constexpr auto STACK_POINTER_SIZE = 2 /* bytes */;
@@ -23,7 +27,7 @@ constexpr auto MAX_NUM_INSTRUCTIONS = (MEMORY_SIZE - PROGRAM_DATA_START_LOCATION
 class CHIP_8
 {
 public:
-	void load_program(const std::array<std::uint16_t, MAX_NUM_INSTRUCTIONS>& program);
+	void load_program(const std::array<instruction_t, MAX_NUM_INSTRUCTIONS>& program);
 	void run();
 
 	CHIP_8()
@@ -32,18 +36,18 @@ public:
 	}
 
 private:
-	std::array<std::uint8_t, MEMORY_SIZE> memory;
-	std::array<std::uint8_t, NUM_REGISTERS> registers;
-	std::array<std::uint16_t, STACK_SIZE / STACK_POINTER_SIZE> stack;
+	std::array<byte, MEMORY_SIZE> memory;
+	std::array<byte, NUM_REGISTERS> registers;
+	std::array<double_byte, STACK_SIZE / STACK_POINTER_SIZE> stack;
 
 	std::array<std::array<bool, FRAME_BUFFER_HEIGHT>, FRAME_BUFFER_WIDTH> frame_buffer;
 
-	std::uint16_t pc;
-	std::uint16_t index_register;
-	std::uint8_t stack_pointer;
+	double_byte pc;
+	double_byte index_register;
+	byte stack_pointer;
 
-	std::uint8_t delay_timer;
-	std::uint8_t sound_timer;
+	byte delay_timer;
+	byte sound_timer;
 
-	void load_fonts(std::uint16_t start_location, const decltype(FONT_DATA)& font_data);
+	void load_fonts(double_byte start_location, const decltype(FONT_DATA)& font_data);
 };
