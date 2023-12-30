8ff7c83b93ed24e5284e1b5c4fb7896fdf1271b5
Abhishek Kumar <abhi.kr.2100@gmail.com>
2023-12-31 00:21:25 +0530

    Export CHIP_8, Keyboard, and Key to PyCHIP8 Python module

--- Python-Frontend/PyCHIP8.cpp
+++ Python-Frontend/PyCHIP8.cpp
@@ -1,14 +1,46 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "CHIP-8.hpp"
+#include "keyboard.hpp"
 
 namespace py = pybind11;
-
-int add(int i, int j)
-{
-	return i + j;
-}
 
 PYBIND11_MODULE(PyCHIP8, m)
 {
 	m.doc() = "CHIP-8 emulator library";
-	m.def("add", &add, "Add two given integers.");
+
+	py::class_<CHIP_8>(m, "CHIP_8")
+		.def(py::init())
+		.def("load_program", &CHIP_8::load_program)
+		.def("run_one", &CHIP_8::run_one)
+		.def_property_readonly("frame_buffer", &CHIP_8::get_frame_buffer)
+		.def("decrement_timers", &CHIP_8::decrement_timers)
+		.def_readonly("keyboard", &CHIP_8::keyboard);
+
+	py::class_<Keyboard>(m, "Keyboard")
+		.def(py::init())
+		.def("set_key_pressed", &Keyboard::set_key_pressed)
+		.def("set_key_released", &Keyboard::set_key_released)
+		.def("is_key_pressed", &Keyboard::is_key_pressed);
+
+	py::enum_<Key>(m, "Key")
+		.value("K0", Key::K0)
+		.value("K1", Key::K1)
+		.value("K2", Key::K2)
+		.value("K3", Key::K3)
+		.value("K4", Key::K4)
+		.value("K5", Key::K5)
+		.value("K6", Key::K6)
+		.value("K7", Key::K7)
+		.value("K8", Key::K8)
+		.value("K9", Key::K9)
+		.value("KA", Key::KA)
+		.value("KB", Key::KB)
+		.value("KC", Key::KC)
+		.value("KD", Key::KD)
+		.value("KE", Key::KE)
+		.value("KF", Key::KF)
+		.value("NONE", Key::NONE)
+		.export_values();
 }
