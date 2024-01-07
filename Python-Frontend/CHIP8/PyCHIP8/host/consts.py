3f384ce84f8b14a188695cac5ca5e8a7a5b40628
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-01-07 18:25:01 +0530

    Switch to PySide6 from Pygame
    In the future, we'd like to add menus, toolbars, etc. PySide6 is more suited to these tasks.

--- Python-Frontend/CHIP8/PyCHIP8/host/consts.py
+++ Python-Frontend/CHIP8/PyCHIP8/host/consts.py
@@ -1,4 +1,4 @@
-import pygame
+from PySide6.QtCore import Qt
 
 from PyCHIP8.PyCHIP8 import Key
 
@@ -6,20 +6,20 @@ from PyCHIP8.PyCHIP8 import Key
 SCALING_FACTOR = 10
 
 KBD_TO_CHIP_8 = {
-    pygame.KSCAN_X: Key.K0,
-    pygame.KSCAN_1: Key.K1,
-    pygame.KSCAN_2: Key.K2,
-    pygame.KSCAN_3: Key.K3,
-    pygame.KSCAN_Q: Key.K4,
-    pygame.KSCAN_W: Key.K5,
-    pygame.KSCAN_E: Key.K6,
-    pygame.KSCAN_A: Key.K7,
-    pygame.KSCAN_S: Key.K8,
-    pygame.KSCAN_D: Key.K9,
-    pygame.KSCAN_Z: Key.KA,
-    pygame.KSCAN_C: Key.KB,
-    pygame.KSCAN_4: Key.KC,
-    pygame.KSCAN_R: Key.KD,
-    pygame.KSCAN_F: Key.KE,
-    pygame.KSCAN_V: Key.KF,
+    Qt.Key.Key_X: Key.K0,
+    Qt.Key.Key_1: Key.K1,
+    Qt.Key.Key_2: Key.K2,
+    Qt.Key.Key_3: Key.K3,
+    Qt.Key.Key_Q: Key.K4,
+    Qt.Key.Key_W: Key.K5,
+    Qt.Key.Key_E: Key.K6,
+    Qt.Key.Key_A: Key.K7,
+    Qt.Key.Key_S: Key.K8,
+    Qt.Key.Key_D: Key.K9,
+    Qt.Key.Key_Z: Key.KA,
+    Qt.Key.Key_C: Key.KB,
+    Qt.Key.Key_4: Key.KC,
+    Qt.Key.Key_R: Key.KD,
+    Qt.Key.Key_F: Key.KE,
+    Qt.Key.Key_V: Key.KF,
 }
--- Python-Frontend/CHIP8/main.py
+++ Python-Frontend/CHIP8/main.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-
 from sys import argv, exit, stderr
-from time import time_ns
 
-import pygame
+from PySide6.QtCore import QTimer
+from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem, QGraphicsPixmapItem
 
-from PyCHIP8.host.consts import SCALING_FACTOR, KBD_TO_CHIP_8
-from PyCHIP8.host.helpers import draw_frame_buffer_on_surface, get_bytes
+from PyCHIP8.host.consts import KBD_TO_CHIP_8, SCALING_FACTOR
+from PyCHIP8.host.helpers import get_bytes
 from PyCHIP8.PyCHIP8 import (
-    CHIP_8, MILLISECONDS_PER_REFRESH,
-    INSTRUCTIONS_PER_REFRESH, TIMER_DECREMENTS_PER_REFRESH
+    CHIP_8, MILLISECONDS_PER_REFRESH, INSTRUCTIONS_PER_REFRESH, TIMER_DECREMENTS_PER_REFRESH
 )
 
 
@@ -24,46 +23,73 @@ def main():
     ins_bytes = get_bytes(filename)
     machine.load_program_from_bytes(ins_bytes)
 
-    width = len(machine.frame_buffer)
-    height = len(machine.frame_buffer[0])
+    app = CHIP8App(machine)
+    return app.exec()
 
-    pygame.init()
-    pygame.display.set_caption("CHIP-8")
-    screen = pygame.display.set_mode((width * SCALING_FACTOR, height * SCALING_FACTOR))
 
-    canvas = pygame.surface.Surface((width, height))
+class CHIP8App(QApplication):
+    def __init__(self, machine):
+        super().__init__([])
+        self.machine = machine
 
-    last_limiter_check_time = time_ns()
-    while True:
-        event = pygame.event.poll()
-        match event.type:
-            case pygame.QUIT:
-                break
-            case pygame.KEYDOWN if event.scancode in KBD_TO_CHIP_8:
-                machine.keyboard.set_key_pressed(KBD_TO_CHIP_8[event.scancode])
-            case pygame.KEYUP if event.scancode in KBD_TO_CHIP_8:
-                machine.keyboard.set_key_released(KBD_TO_CHIP_8[event.scancode])
+        self.scene = CHIP8FrameBuffer()
+        self.screen = CHIP8Screen(self.scene, self.machine)
+        self.screen.scale(SCALING_FACTOR, SCALING_FACTOR)
 
-        curr_time = time_ns()
-        millis_elapsed = int((curr_time - last_limiter_check_time) / 1e6)
+        self.refresh_timer = QTimer()
+        self.refresh_timer.setInterval(MILLISECONDS_PER_REFRESH)
+        self.refresh_timer.timeout.connect(self.refresh)
+        self.refresh_timer.start()
 
-        if millis_elapsed < MILLISECONDS_PER_REFRESH:
-            continue
-        last_limiter_check_time = curr_time
+        self.screen.show()
 
-        refreshes_elapsed = int(millis_elapsed / MILLISECONDS_PER_REFRESH)
+    def refresh(self):
+        for _ in range(INSTRUCTIONS_PER_REFRESH):
+            self.machine.run_one()
+        self.machine.decrement_timers(TIMER_DECREMENTS_PER_REFRESH)
+        self.scene.refresh(self.machine.frame_buffer)
+        self.screen.update()
 
-        ins_elapsed = refreshes_elapsed * INSTRUCTIONS_PER_REFRESH
-        for _ in range(ins_elapsed):
-            machine.run_one()
 
-        timer_decrements_elapsed = refreshes_elapsed * TIMER_DECREMENTS_PER_REFRESH
-        machine.decrement_timers(timer_decrements_elapsed)
+class CHIP8Screen(QGraphicsView):
+    def __init__(self, scene, machine):
+        super().__init__(scene)
+        self.machine = machine
 
-        draw_frame_buffer_on_surface(canvas, machine.frame_buffer)
-        screen.blit(pygame.transform.scale(canvas, screen.get_size()), (0, 0))
-        pygame.display.flip()
+        width = len(self.machine.frame_buffer) * (SCALING_FACTOR + 1)
+        height = len(self.machine.frame_buffer[0]) * (SCALING_FACTOR + 1)
+        self.setFixedSize(width, height)
+
+    def keyPressEvent(self, event):
+        if (key := event.key()) in KBD_TO_CHIP_8:
+            self.machine.keyboard.set_key_pressed(KBD_TO_CHIP_8[key])
+
+    def keyReleaseEvent(self, event):
+        if (key := event.key()) in KBD_TO_CHIP_8:
+            self.machine.keyboard.set_key_released(KBD_TO_CHIP_8[key])
+
+
+class CHIP8FrameBuffer(QGraphicsScene):
+    def __init__(self):
+        super().__init__()
+        self.addText("Empty!")
+
+    def refresh(self, new_frame_buffer):
+        self.clear()
+
+        width = len(new_frame_buffer)
+        height = len(new_frame_buffer[0])
+
+        img = QImage(width, height, QImage.Format.Format_Mono)
+        for x, row in enumerate(new_frame_buffer):
+            for y, pixel in enumerate(row):
+                img.setPixel(x, y, 0 if pixel else 1)
+
+        pixmap = QPixmap.fromImage(img)
+        self.addItem(QGraphicsPixmapItem(pixmap))
+
+        self.update()
 
 
 if __name__ == '__main__':
-    main()
+    exit(main())
