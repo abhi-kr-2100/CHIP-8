018a69806900d29a43f12b83539b872c4a279202
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-01-07 19:00:39 +0530

    Reduce coupling between widgets

--- Python-Frontend/CHIP8/main.py
+++ Python-Frontend/CHIP8/main.py
@@ -28,13 +28,14 @@ def main():
 class CHIP8App(QApplication):
     def __init__(self, machine):
         super().__init__([])
+
         self.machine = machine
 
-        self.scene = CHIP8GameScreenScene()
-        self.screen = CHIP8GameScreen(self.scene, self.machine)
-        self.screen.scale(SCALING_FACTOR, SCALING_FACTOR)
+        width = len(self.machine.frame_buffer)
+        height = len(self.machine.frame_buffer[0])
+        self.screen = CHIP8GameScreen(width, height, SCALING_FACTOR)
 
-        self.main_window = CHIP8MainWindow(self.screen)
+        self.main_window = CHIP8MainWindow(self.screen, self.machine.keyboard)
 
         self.refresh_timer = QTimer()
         self.refresh_timer.setInterval(MILLISECONDS_PER_REFRESH)
@@ -47,36 +48,41 @@ class CHIP8App(QApplication):
         for _ in range(INSTRUCTIONS_PER_REFRESH):
             self.machine.run_one()
         self.machine.decrement_timers(TIMER_DECREMENTS_PER_REFRESH)
-        self.scene.refresh(self.machine.frame_buffer)
-        self.screen.update()
+        self.screen.refresh(self.machine.frame_buffer)
 
 
 class CHIP8MainWindow(QMainWindow):
-    def __init__(self, game_screen):
+    def __init__(self, game_screen, keyboard):
         super().__init__()
 
         self.game_screen = game_screen
+        self.keyboard = keyboard
+
         self.setCentralWidget(game_screen)
-
         self.setWindowTitle("CHIP-8")
-
-
-class CHIP8GameScreen(QGraphicsView):
-    def __init__(self, scene, machine):
-        super().__init__(scene)
-        self.machine = machine
-
-        width = len(self.machine.frame_buffer) * (SCALING_FACTOR + 1)
-        height = len(self.machine.frame_buffer[0]) * (SCALING_FACTOR + 1)
-        self.setFixedSize(width, height)
 
     def keyPressEvent(self, event):
         if (key := event.key()) in KBD_TO_CHIP_8:
-            self.machine.keyboard.set_key_pressed(KBD_TO_CHIP_8[key])
+            self.keyboard.set_key_pressed(KBD_TO_CHIP_8[key])
 
     def keyReleaseEvent(self, event):
         if (key := event.key()) in KBD_TO_CHIP_8:
-            self.machine.keyboard.set_key_released(KBD_TO_CHIP_8[key])
+            self.keyboard.set_key_released(KBD_TO_CHIP_8[key])
+
+
+class CHIP8GameScreen(QGraphicsView):
+    def __init__(self, width, height, scaling_factor):
+        super().__init__()
+
+        self.game_scene = CHIP8GameScreenScene()
+        self.setScene(self.game_scene)
+
+        self.setFixedSize(width * (scaling_factor + 1), height * (scaling_factor + 1))
+        self.scale(scaling_factor, scaling_factor)
+
+    def refresh(self, frame_buffer):
+        self.game_scene.refresh(frame_buffer)
+        self.update()
 
 
 class CHIP8GameScreenScene(QGraphicsScene):
