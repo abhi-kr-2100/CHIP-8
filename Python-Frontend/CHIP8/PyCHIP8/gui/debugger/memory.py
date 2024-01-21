d0f9ae6cb84eae80d371de188c5af7a8017d3ec9
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-01-21 23:38:29 +0530

    Fix typo in function name

--- Python-Frontend/CHIP8/PyCHIP8/gui/debugger/memory.py
+++ Python-Frontend/CHIP8/PyCHIP8/gui/debugger/memory.py
@@ -9,9 +9,9 @@ class MemoryModel(QStringListModel):
     def __init__(self):
         super().__init__()
 
-        debugger.on_exec(self.refresh_if_nedded)
+        debugger.on_exec(self.refresh_if_needed)
 
-    def refresh_if_nedded(self, _, instruction):
+    def refresh_if_needed(self, _, instruction):
         if not affects_memory(instruction):
             return
         items = [hex(item) for item in debugger.memory]
